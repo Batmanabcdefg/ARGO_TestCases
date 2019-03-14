5edf99acd7a3ba99bd02d960af0605a3d3e41569
SanaAmri <40390340+SanaAmri@users.noreply.github.com>
2019-03-14 01:22:25 +0100

    Add files via upload

---
+++ Codes/2D_Half_Cylinder_DiffTermBR2/CaseRefinedMesh/7424_Elements/Test1_tol_e-8/Output/Solution/plotdata.m
@@ -0,0 +1,49 @@
+%Matlab script
+
+% Plot the Density at the wall
+D=importdata('Density_Wall.dat');
+x_d = D.data(:,1);
+angle = linspace(-pi/2,pi/2,length(x_d));
+d = D.data(:,4);
+
+figure(1)
+plot(angle,d,'.')
+ylabel('Density [kg·m^−^2]')
+xlabel('Angle [radian]')
+xlim([-pi/2 pi/2])
+
+
+
+P=importdata('TotalPressure_Wall.dat');
+x_p = P.data(:,1);
+angle = linspace(-pi/2,pi/2,length(x_p));
+p = P.data(:,4);
+
+figure(2)
+plot(angle,p,'.')
+ylabel('Total Pressure [Pa]')
+xlabel('Angle [radian]')
+xlim([-pi/2 pi/2])
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% R E S I D U A L 
+
+R=importdata('residual.dat'); %iteration/ wall time/ residual norm / cfl
+x_r = R.data(:,1);
+r = R.data(:,3);
+cfl = R.data(:,4);
+
+figure(3)
+yyaxis left
+semilogy(x_r,r,'.')
+ylim([0 10^(10)]) 
+ylabel('Residual Norm L_2 (log_1_0 scale) ')
+xlabel('Iteration')
+
+yyaxis right
+semilogy(x_r, cfl,'.')
+ylim([0 10^(5)])
+ylabel('CFL')
+
+
